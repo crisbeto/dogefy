352a5fb73a08bbe9d30420f847fd7f50f7778928
crisbeto <kristiyan@basidia.dk>
2015-10-24 11:41:47 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# dogefy
+# Dogefy
 ASCII Doge for your command line
+
+## Command line usage
+
+Install via `npm install doge -g`.
+
+Run with `doge Lorem ipsum dolor`.
+
+Result:
+```
+Lorem                ▄              ▄
+                    ▌▒█           ▄▀▒▌
+                    ▌▒▒█        ▄▀▒▒▒▐
+                   ▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+                 ▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+               ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌ much do wow
+              ▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌
+              ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+             ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌
+             ▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌
+            ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐
+            ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌
+            ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐
+such ipsum   ▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌
+             ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐
+              ▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌
+                ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀
+                  ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀
+                     ▒▒▒▒▒▒▒▒▒▒▀▀
+```
+
+## Normal usage
+You can also include this in your project with `npm install dogefy --save`.
+
+Aftewards you can require it:
+
+```javascript
+var doge = require('dogefy');
+console.log(doge('Lorem ipsum dolor'));
+```
+
---
+++ doge.js
@@ -0,0 +1,77 @@
+Array.prototype.getRandomItem = function(){
+    return this[this.getRandomIndex()];
+};
+
+Array.prototype.getRandomIndex = function(){
+    return Math.floor(Math.random()*this.length);
+};
+
+module.exports = function(text){
+    if(!text) return;
+
+    var wow = [
+    '           ▄              ▄',
+    '          ▌▒█           ▄▀▒▌',
+    '          ▌▒▒█        ▄▀▒▒▒▐',
+    '         ▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐',
+    '       ▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐',
+    '     ▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌',
+    '    ▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒▌',
+    '    ▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐',
+    '   ▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄▌',
+    '   ▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒▌',
+    '  ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒▐',
+    '  ▐▒▒▐▀▐▀▒░▄▄▒▄▒▒▒▒▒▒░▒░▒░▒▒▒▒▌',
+    '  ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒▒▒░▒░▒░▒▒▐',
+    '   ▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒░▒░▒░▒░▒▒▒▌',
+    '   ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▒▄▒▒▐',
+    '    ▀▄▒▒▒▒▒▒▒▒▒▒▒░▒░▒░▒▄▒▒▒▒▌',
+    '      ▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀',
+    '        ▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀',
+    '           ▒▒▒▒▒▒▒▒▒▒▀▀'
+    ];
+    var blankChar = ' ';
+    var cleaned = text.replace(/[^\w\s]|_/g, '').replace(/\s+/g, ' ').split(' ');
+    var blankSpaceRegex = new RegExp(blankChar + '*');
+    var padding = 1;
+    var pool = ['wow'];
+    var prefixes = ['such', 'much', 'very', 'many'];
+
+    (function(){
+        var i = Math.min(cleaned.length, Math.round(wow.length/3));
+
+        while(i-- && cleaned.length){
+            var currentIndex = cleaned.getRandomIndex();
+            pool.push((Math.random() < 0.7 ? (prefixes.getRandomItem() + ' ') : '') + cleaned[currentIndex]);
+            cleaned.splice(currentIndex, 1);
+        }
+    })();
+
+    pool = pool.sort(function(a, b){
+        return b.length - a.length;
+    });
+
+    var extra = new Array(pool[0].length + padding).join(blankChar);
+
+    wow.forEach(function(item, index){
+        wow[index] = extra + item + extra;
+    });
+
+    pool.forEach(function(item){
+        var dogeIndex = wow.getRandomIndex();
+        var isInfront = Math.random() > 0.5;
+        var value = wow[dogeIndex];
+        var availableBefore = value.match(blankSpaceRegex)[0].length - padding;
+
+        if(isInfront && availableBefore >= item.length){
+            value = item + value.substring(item.length, value.length);
+        }else{
+            value = value.substring(0, value.length - item.length) + blankChar + item;
+        }
+
+        wow[dogeIndex] = value;
+    });
+
+    return wow.join('\n');
+};
+
